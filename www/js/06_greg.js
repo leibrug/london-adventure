0b5e1773d94a3c8e3597d899b36dce3168d2a8f2
Michał Gurbiel <lachim@leibrug.pl>
2016-09-05 15:23:07 +0200

    Greg: moving frame

--- www/js/06_greg.js
+++ www/js/06_greg.js
@@ -7,13 +7,14 @@ var gGreg = function() {
   var animationFrame;
 
 
-  var framePositions = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8]
-  ];
-  var framePosition = 1;
+
+
+  var framePositionX = 0;
+  var framePositionY = 0;
   var frameState = 0;
   var frameStyles = ['#dcb42c', '#f8f478'];
+  var frameMoveTimeStamp = 0;
+  var frameMoveDirection;
 
   // Set up stroke for frame
 
@@ -22,20 +23,107 @@ var gGreg = function() {
 
 
 
+
+
   var previousTimeStamp = 0;
 
 
 
   function draw(timeStamp) {
+    // TODO: draw anything only on changes
+
+    c.strokeStyle = '#000000';
+    c.strokeRect(40, 60, 240, 120);
     c.drawImage(gfxBanknote, 40, 60);
 
-    if (isKeyDown && currentKey === 32) {
-      frameState = Math.abs(frameState - 1);
-      c.strokeStyle = frameStyles[frameState];
+    var frameOffsetX = 0;
+    var frameOffsetY = 0;
+
+    // TODO block if during frame move
+    if (isKeyDown) {
+      if (currentKey === 32) {
+        frameState = Math.abs(frameState - 1);
+        c.strokeStyle = frameStyles[frameState];
+      }
+      else {
+        var canMove = false;
+        switch (currentKey) {
+          case 37:
+            canMove = framePositionX > 0;
+            frameMoveDirection = '←';
+            break;
+          case 38:
+            canMove = framePositionY > 0;
+            frameMoveDirection = '↑';
+            break;
+          case 39:
+            canMove = framePositionX < 3;
+            frameMoveDirection = '→';
+            break;
+          case 40:
+            canMove = framePositionY < 1;
+            frameMoveDirection = '↓';
+            break;
+        }
+        if (canMove) {
+          frameMoveTimeStamp = timeStamp;
+          if (frameState === 0) {
+
+          }
+          else {
+            // TODO: move pieces
+          }
+        }
+        else if (frameState === 1) {
+          // TODO: reset if frame state is 1 and can't move
+        }
+      }
       isKeyDown = false;
     }
-    c.strokeRect(40, 60, 60, 60);
 
+    if (frameMoveTimeStamp !== 0) {
+      var delta = timeStamp - frameMoveTimeStamp;
+      if (delta < 250) {
+        var offset = Math.min(delta/4.17, 60);
+
+        switch (frameMoveDirection) {
+          case '←':
+            frameOffsetX = -offset;
+            break;
+          case '↑':
+            frameOffsetY = -offset;
+            break;
+          case '→':
+            frameOffsetX = offset;
+            break;
+          case '↓':
+            frameOffsetY = offset;
+            break;
+        }
+      }
+      else {
+        frameMoveTimeStamp = 0;
+        switch (frameMoveDirection) {
+          case '←':
+            framePositionX -= 1;
+            break;
+          case '↑':
+            framePositionY -= 1;
+            break;
+          case '→':
+            framePositionX += 1;
+            break;
+          case '↓':
+            framePositionY += 1;
+            break;
+        }
+      }
+    }
+
+    c.strokeStyle = frameStyles[frameState];
+    c.strokeRect(40 + framePositionX * 60 + frameOffsetX, 60 + framePositionY * 60 + frameOffsetY, 60, 60);
+
+    // previousTimeStamp = timeStamp;
     window.requestAnimationFrame(draw);
   }
 
